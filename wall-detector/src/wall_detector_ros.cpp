793733bfe2bc058fa58b07462f206eedb5b6cb4a
Christopher Bjørge <chrisbjoe@hotmail.com>
2025-02-14 17:24:43 +0000

    initial code for wall detection, not finished

---
+++ wall-detector/src/wall_detector_ros.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+
+int main ()
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Fill in the cloud data
+  cloud->width  = 15;
+  cloud->height = 15;
+  cloud->points.resize (cloud->width * cloud->height);
+
+  // Generate the data
+  for (auto& point: *cloud)
+  {
+    point.x = 1.0;
+    point.y = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.z = 1024 * rand () / (RAND_MAX + 1.0f);
+  }
+
+  // Set a few outliers
+  (*cloud)[0].x = 2.0;
+  (*cloud)[3].x = -2.0;
+  (*cloud)[6].x = 4.0;
+
+  std::cerr << "Point cloud data: " << cloud->size () << " points" << std::endl;
+  for (const auto& point: *cloud)
+    std::cerr << "    " << point.x << " "
+                        << point.y << " "
+                        << point.z << std::endl;
+
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  //Define axis for vertical planes 
+  Eigen::Vector3F axis(1.0, 0.0, 0.0); //Normal to vertical planes
+  seg.setAxis(axis);
+  seg.setEpsAngle(pcl::deg2rad(10.0)); //Allow up to 10 degree deviation
+
+  seg.setInputCloud (cloud);
+  seg.segment (*inliers, *coefficients);
+
+  if (inliers->indices.size () == 0)
+  {
+    PCL_ERROR ("Could not estimate a planar model for the given dataset.\n");
+    return (-1);
+  }
+
+  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+  for (const auto& idx: inliers->indices)
+    std::cerr << idx << "    " << cloud->points[idx].x << " "
+                               << cloud->points[idx].y << " "
+                               << cloud->points[idx].z << std::endl;
+
+  return (0);
+}
